65305af36d04d8da5391804ce9db087b6a7ea059
RomanKosov <kosovr48@gmail.com>
2024-01-05 16:55:38 +0500

    Add Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿
+bool CheckElements(string[] array, int index)
+{
+    if (index < 0 || index >= array.Length)
+        return false;
+    return array[index].Length <= 3;
+}
+
+int CountElements(string [] array)
+{
+    int total = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (CheckElements(array, i) == true)
+        {
+            total++;
+        }
+    }
+    return total;
+}
+
+void newArray(string[] array, int count)
+{
+    string[] newArray = new string[count];
+    count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (CheckElements(array, i) == true)
+        {
+            string temp = array[i];
+            newArray[count] = temp;
+            count++;
+        }
+    }
+    Console.WriteLine(String.Join(" ", newArray));
+
+}
+
+Console.WriteLine("Введите строку, состоящую из слов, разделенных пробелами:");
+string[] array = Console.ReadLine().Split(" ").ToArray();
+int count = CountElements(array);
+newArray(array, count);
